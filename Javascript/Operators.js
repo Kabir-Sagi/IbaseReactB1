ec2af090a0806db68065d16f8cefd73df74137c8
TheSaGiii <sagithedeveloper@gmail.com>
2021-11-02 09:55:36 +0530

    updated operators code

---
+++ Javascript/Operators.js
@@ -0,0 +1,29 @@
+var a =10
+var b = 20
+var c = "10"
+var d;
+
+// console.log(a==b)  //false
+// console.log(a>=c)  // true
+console.log(d)
+d = a 
+console.log(d)
+a+=a // a = a+ a = 20
+b*=b // b = b*b  400
+b+=a // b = b +a = 400 + 20 = 420
+// console.log(a)
+// console.log(b)
+var x = 100
+var y = 500
+
+//    console.log(x < 200 && y > 400 ? "Ibase" :"Javascript")
+
+var a1 = "ibase"
+var a2 = 10
+var a3 = true 
+var a4;
+
+    console.log(a1,typeof(a1))
+    console.log(a2,typeof(a2))
+    console.log(a3,typeof(a3))
+    console.log(a4,typeof(a4))
